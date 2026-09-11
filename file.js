f7edd17a9526ef650812392aba10641a8dd498af
Galaxy-368 <binuadarsana@gmail.com>
2026-09-12 04:39:08 +0530

    updated the api

--- file.js
+++ file.js
@@ -38,7 +38,7 @@ form.addEventListener("submit", async function (event) {
 
         // Send data to Python ML model
 
-        const response = await fetch("http://127.0.0.1:5000/predict", {
+        const response = await fetch("https://useless-project-rm33.onrender.com/predict", {
 
             method: "POST",
 
